04819256a40cceade396cfbbc2035da652b07e78
Alex-Fedd <blackyblackmail@yandex.ru>
2024-07-02 06:09:32 +0300

    5 - правки

--- src/main/kotlin/CurrentScreenState.kt
+++
@@ -1,7 +0,0 @@
-enum class CurrentScreenState(val nameOf: String) {
-    ArchiveSelection("меню списка архивов"),
-    ArchiveCreation("меню создания архивов"),
-    NoteSelection("меню списка заметок"),
-    NoteCreation("меню создания заметок"),
-    NoteScreen("заметке")
-}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,71 +1,156 @@
 import java.util.Scanner
+import kotlin.system.exitProcess
 
-//пока что общий файл - потом на классы в разные файлы разобью
 
 fun main() {
     println("Запускаем приложение \"Заметки\"")
-    val startApp = MenuSelect()
-    startApp.showArchivesMenu()
+    val startApp = MainMenu()
+    startApp.startArchivesMenu()
 }
 
 
-class MenuSelect { // пока здесь логику архивов начальную и выбор экранов пишу
+class MainMenu {
     private val scan = Scanner(System.`in`)
-
-    val archivesList = mutableListOf<Archive>()
-    private val screenState: CurrentScreenState? = CurrentScreenState.ArchiveSelection//меню архивов
-    private val listOfCommands = mutableListOf(
-        "Создать архив",
-        "Выйти"
+    private val archivesList = mutableListOf<Archive>()
+    private val menuCommandsList = mutableListOf(
+        MenuCommand("Создать архив") { createAnArchive() },
+        MenuCommand("Выйти") { leave() },
     )
 
-    fun showArchivesMenu(): CurrentScreenState? {
-        while (true) {
-            println("Вы в ${screenState?.nameOf}. Введите команду (цифру) из списка:")
-
-            for (i in 0 until listOfCommands.size) {
-                println("$i. ${listOfCommands[i]}")
-            }
-
-            if (scan.hasNextInt()) {
-                val userInput = scan.nextInt()
-                if(userInput < 0){
-                    println("Вы ввели отрицательное число")
-                }
-                else if(userInput !in 0 until listOfCommands.size){
-                    println("Такой команды (цифры) нет на экране")
-                }
-
-                else{
-                    when(userInput) {
-                        0 -> return CurrentScreenState.ArchiveCreation
-                        listOfCommands.size - 1 -> return null // выход обратно
-                        else -> checkArchiveNumber(userInput)
-                    }
-                }//в "элсе" выше проверка команды существующей - 1, 2, ... откроют архив под этим номером
-
-            }//if проверки числа кончается
-
-            else {
-                println("Вы ввели нечисловое значение. Введите цифру, обозначающую команду")
-                scan.next()
-            }
-
+    private fun printTheInitialCommands() {
+        println("Вы в списке архивов. Выберите нужную команду")
+        for (i in 0..1) {
+            println("$i. ${menuCommandsList[i].command}")
         }
     }
-    private fun checkArchiveNumber(userInput: Int): Note? {
-        return Note("Заметка", "Какой-то контент внутри")
+
+    fun startArchivesMenu() {
+        while (true) {
+            printTheInitialCommands()
+            if (menuCommandsList.size > 2) {
+                for (i in 2 until menuCommandsList.size) {
+                    println("$i. ${menuCommandsList[i].command}")
+                }
+            }
+            menuCheck(input = scan.nextLine(), menuCommandsList)
+        }
+    }
+
+
+    fun menuCheck(input: String, menuCommandsList: MutableList<MenuCommand>) {
+
+            when (input) {
+                "0" -> menuCommandsList[0].commandAction()
+                "1" -> menuCommandsList[1].commandAction()
+                else -> {
+                    if (input == "" || input.toIntOrNull() == null) {
+                        println("Вы ввели нечисловое значение. Введите корректный номер.")
+                    } else if (input.toIntOrNull() !in 0 until menuCommandsList.size) {
+                        println("Такой команды нет. Введите корректное значение из списка.")
+                    } else {
+                        menuCommandsList[input.toInt()].commandAction() // запускаю команду созданного элемента, если все ок
+                    }
+                }
+            }
+
+    }
+
+
+    private fun leave() {
+        exitProcess(0)
+    }
+
+    private fun createAnArchive() {
+        println("Задайте название новому архиву")
+        val userInput = scan.nextLine()
+        if(userInput != "" && userInput != null && userInput.isNotBlank()){
+            archivesList.add(Archive(userInput, mutableListOf<Note>()))
+            menuCommandsList.add(MenuCommand("Архив $userInput") { openSelectedArchive(userInput) })
+            println("Архив $userInput создан")
+            for(i in archivesList){ println(i.archiveName) } //check for printed archives
+        } else {
+            println("Архив должен иметь название (пустое значение не принимается)")
+        }
+    }
+
+    private fun openSelectedArchive(archiveName: String) {
+        println("Открыт архив $archiveName.")
+        NotesMenu(this).printNotesMenu()
     }
 
 }
 
+
+class NotesMenu(private val referenceToMainMenu: MainMenu){ // создаю функции заметок и пытаюсь вернуться, сохрнаить и тд, взаимодейст с архивом
+    private val notesList = mutableListOf<Note>()
+    private val scanner = Scanner(System.`in`)
+    private val menuCommandsList = mutableListOf(
+        MenuCommand("Создать заметку") { createANote() },
+        MenuCommand("Вернуться") { leave() },
+    )
+
+    private fun leave() {
+        referenceToMainMenu.startArchivesMenu()
+    }
+
+
+    private fun menuChecker() {
+        val input = scanner.nextLine()
+        referenceToMainMenu.menuCheck(input = input, menuCommandsList)
+        // createANote()
+    }
+
+
+    private fun createANote() { // тут создаю заметку и даю ей название и (держу связь с архивом ?)
+        println("Задайте название новой заметке")
+        val userInput = scanner.nextLine()
+        if(userInput != "" && userInput != null && userInput.isNotBlank()){
+
+            println("Введите свой текст для заметки")
+            val contentInputForNote = scanner.nextLine()
+            if(contentInputForNote == null || contentInputForNote == ""){
+               println("Заметка не может быть пустой")
+            }
+            val myNote = Note(contentInputForNote)
+            notesList.add(myNote)
+            menuCommandsList.add(MenuCommand("Заметка $userInput") { openTheNote(userInput) })
+            println("Заметка $userInput создана")
+            for(i in notesList){ println(i.noteName) } //check for printed archives
+        } else {
+            println("Заметка должна иметь название (пустое значение не принимается)")
+        }
+    }
+
+
+    private fun openTheNote(input: String){
+
+    }
+
+
+    fun printNotesMenu(){
+       while(true){
+           println("Выберите заметку или создайте новую, выбрав нужную команду.")
+           for(i in 0..1){
+               println("$i. ${menuCommandsList[i].command}")
+           }
+           if (menuCommandsList.size > 2) {
+               for (i in 2 until menuCommandsList.size) {
+                   println("$i. ${menuCommandsList[i].command}")
+               }
+           }
+
+           menuChecker()
+       }
+    }
+
+}
 
 class Archive(val archiveName: String, val notesList: MutableList<Note>)
 
-class Note(val noteName: String, val noteData: String)
-
-
-
-interface Selections {
-    fun someSharedFunction()
+class Note(val noteName: String){
+    val noteData: String = ""
 }
+
+class MenuCommand(val command: String, val commandAction: () -> Unit)
+
+
